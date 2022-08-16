bce211c55ef3a805a6c6ae7b7f6af7011e7860c0
lisp719 <368034+lisp719@users.noreply.github.com>
2022-08-16 10:54:19 +0000

    feat: Migrate to Astro

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "MoguraWatch";
+export const SITE_DESCRIPTION = "It is a site to record what I watch or study.";
